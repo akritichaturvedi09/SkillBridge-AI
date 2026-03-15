138c834be15ec1186dd4054278f64998f9770a47
Akriti <ankur@Ankurs-MacBook-Air.local>
2026-03-15 12:31:25 +0530

    chore: deployment fixes

--- Backend/server.js
+++ Backend/server.js
@@ -2,16 +2,15 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
-connectToDB()
-    .then((res)=>{
-        if (process.env.NODE_ENV !== "production") {
-            app.listen(process.env.PORT || 3000,()=>{
-                console.log("Server Started")
-            })
-        }
-    })
-    .catch((err)=>{
-        console.log(err+"\nServer Down")
-    })
+// Connect to DB (non-blocking for serverless)
+connectToDB().catch((err) => {
+    console.error("DB connection error:", err)
+})
 
-module.exports = app;
+if (process.env.NODE_ENV !== "production") {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log("Server Started")
+    })
+}
+
+module.exports = app
